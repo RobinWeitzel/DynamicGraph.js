f967c6ca5fe060a558e19dbc34320c5015d06145
RobinWeitzel <robin.weitzel.rw@gmail.com>
2017-10-20 16:35:50 +0200

    Fixed bug: Variable labels had been renamed to xLabels but this change was not applied to all functions.

--- src/dynamicgraph.js
+++ src/dynamicgraph.js
@@ -1,3 +1,12 @@
+﻿/**
+ * Copyright 2017 Robin Weitzel
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * See https://github.com/RobinWeitzel/DynamicGraph.js for more information
+ */
+
 let globalMeta;
 
 /**
@@ -783,8 +792,8 @@ class DynamicGraph {
                 dataset = {
                     label: y.label,
                     data: Array.from(helper.values()).map(h => customReduce(Array.from(h), pos, y)),
-                    backgroundColor: getColorScheme(color, 0.2, labels.length), 
-                    borderColor: getColorScheme(color, 1, labels.length),
+                    backgroundColor: getColorScheme(color, 0.2, xLabels.length), 
+                    borderColor: getColorScheme(color, 1, xLabels.length),
                     borderWidth: 1
                 }
             } else {
@@ -794,8 +803,8 @@ class DynamicGraph {
                 dataset = {
                     label: y,
                     data: Array.from(helper.values()).map(h => customReduce(Array.from(h), pos)),
-                    backgroundColor: getColorScheme(color, 0.2, labels.length),
-                    borderColor: getColorScheme(color, 1, labels.length),
+                    backgroundColor: getColorScheme(color, 0.2, xLabels.length),
+                    borderColor: getColorScheme(color, 1, xLabels.length),
                     borderWidth: 1
                 } 
             }
